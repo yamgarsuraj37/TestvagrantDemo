b0a327320746f06b88db4e5c326e963afe7851e7
yamgarsuraj37 <yamgarsuraj37@gmail.com>
2023-02-22 21:54:30 +0530

    Revert to 9a1f3f9fb3bb77a6782916b57d9f7fcd2a7d152c

--- src/main/java/pom/objects/WikipediaPage.java
+++ src/main/java/pom/objects/WikipediaPage.java
@@ -1,30 +1,30 @@
-package pomObjects;
+package pom.objects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Wikipediapage {
+public class WikipediaPage {
 	public WebDriver driver;
 
-	public Wikipediapage(WebDriver driver) {
+	public WikipediaPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
 	@FindBy(xpath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[12]/td/div/ul/li")
-	private WebElement releasedate2;
+	private WebElement releaseDate2;
 
 	@FindBy(xpath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[14]/td")
-	private WebElement country2;
+	private WebElement countryOfOrigin2;
 
-	public WebElement Releasedate() {
-		return releasedate2;
+	public WebElement releaseDateWikpdia() {
+		return releaseDate2;
 	}
 
-	public WebElement country() {
-		return country2;
+	public WebElement countryWikpdia() {
+		return countryOfOrigin2;
 	}
 
 }
